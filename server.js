85ed022cab428e1073947f8a0302dc2c8f7629ed
Naveen Kumar <naveenbhatpahari@gmail.com>
2025-10-02 01:04:52 +0530

    Payemet System Added

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.json({ extended: false }));
 app.use('/api', require('./routes/api'));
 app.use('/api/users', require('./routes/users.api')); // THIS LINE IS NEW
 app.use('/api/books', require('./routes/books.api'));
+app.use('/api/fees', require('./routes/fees.api')); // ADD THIS LINE
+app.use('/api/payment', require('./routes/payment.api')); // ADD THIS LINE
 app.use('/api/issues', require('./routes/issues.api'));
 app.use('/api/communications', require('./routes/communications.api'));
 app.use('/api/notifications', require('./routes/notifications.api'));
